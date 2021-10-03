b85fbc45e724e4b0ca5bdd495d92f19903cd230e
kungfux <alexander.v.fuks@gmail.com>
2021-10-03 19:47:56 +0300

    Update webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,7 +4,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: ['./src/index.ts'],
-  watch: true,
+  watch: false,
   target: 'node',
   externals: [
     nodeExternals({
