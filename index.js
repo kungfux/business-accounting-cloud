e810f5877cf82c4f3e67ccd25665ca218c0611b7
kungfux <alexander.v.fuks@gmail.com>
2021-10-03 20:58:05 +0300

    Extract root route

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')({
+    logger: true
+});
 
-fastify.route({
-    method: 'GET',
-    url: '/',
-    schema: {
-        querystring: {
-            name: { type: 'string' }
-        },
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    server: { type: 'string' }
-                }
-            }
-        }
-    },
-    preHandler: async (request, reply) => {
-        // TODO: Add authentication
-    },
-    handler: async (request, reply) => {
-        return { server: 'Business Accounting Cloud 1.0.0' }
-    }
-})
+fastify.register(require('./routes/root'));
 
 const start = async () => {
     try {
-        await fastify.listen(80);
+        await fastify.listen(80)
     } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
+        fastify.log.error(err)
+        process.exit(1)
     }
 }
 
-start();
+start()
