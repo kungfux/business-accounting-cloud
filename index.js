c0a03057c8ff9880ddb94d614aee27dc3d0f7cd7
kungfux <alexander.v.fuks@gmail.com>
2021-10-03 20:36:37 +0300

    Add a route

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const fastify = require('fastify')({ logger: true })
 
-fastify.get('/', async (request, reply) => {
-    return { welcome: 'Business Accounting Cloud 1.0.0' }
-});
+fastify.route({
+    method: 'GET',
+    url: '/',
+    schema: {
+        querystring: {
+            name: { type: 'string' }
+        },
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    server: { type: 'string' }
+                }
+            }
+        }
+    },
+    preHandler: async (request, reply) => {
+        // TODO: Add authentication
+    },
+    handler: async (request, reply) => {
+        return { server: 'Business Accounting Cloud 1.0.0' }
+    }
+})
 
 const start = async () => {
     try {
-        await fastify.listen(80)
+        await fastify.listen(80);
     } catch (err) {
-        fastify.log.error(err)
-        process.exit(1)
+        fastify.log.error(err);
+        process.exit(1);
     }
 }
 
