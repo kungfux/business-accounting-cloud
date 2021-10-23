efe91d5ea876e76b9b669239c2ae9ad3921fd0e6
kungfux <alexander.v.fuks@gmail.com>
2021-10-23 14:57:42 +0300

    Update default test web server

--- hostApp.sh
+++ hostApp.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-http-server -p 80 ./client/dist/bac -a 0.0.0.0 -d 0 -g 1 --no-dotfiles --cors
+#http-server -p 80 ./client/dist/bac -a 0.0.0.0 -d 0 -g 1 --no-dotfiles --cors
+ws --spa index.html --directory ./client/dist/bac --log.format dev --port 80
+#--cors.origin "*"
