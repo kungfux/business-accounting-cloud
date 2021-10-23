8186855c21a8c6c0f4b149d04519f1695be8df6e
kungfux <alexander.v.fuks@gmail.com>
2021-10-24 02:56:32 +0300

    Merge API schema definitions

--- server/schemas/company.js
+++ server/schemas/company.js
@@ -1,90 +1,26 @@
 'use strict'
 
-const findAll = {
-  response: {
-    200: {
-      type: 'array',
-      items: {
-        properties: {
-          id: { type: 'integer' },
-          name: { type: 'string' },
-          logo: { type: 'string' },
-          enabled: { type: 'boolean' },
-          created: { type: 'string' }
-        }
-      }
-    }
+const BaseSchema = require('./baseSchema')
+
+var schema = new BaseSchema({
+  id: { type: 'integer' },
+  created: { type: 'string' }
+},
+  {
+    name: { type: 'string' },
+    logo: { type: 'string' },
+    enabled: { type: 'boolean' }
   },
-  querystring: {
-    limit: { type: 'integer' },
-    offset: { type: 'integer' }
-  }
-}
+  {
+    name: { type: 'string' },
+    logo: { type: 'string' },
+    enabled: { type: 'boolean' }
+  })
 
-const findOne = {
-  response: {
-    200: {
-      type: 'object',
-      properties: {
-        id: { type: 'integer' },
-        name: { type: 'string' },
-        logo: { type: 'string' },
-        enabled: { type: 'boolean' },
-        created: { type: 'string' }
-      }
-    },
-    404: {
-      type: 'object',
-      properties: {
-        message: { type: 'string' }
-      }
-    }
-  },
-  params: {
-    type: 'object',
-    properties: {
-      id: { type: 'integer' }
-    }
-  }
-}
-
-const insertOne = {
-  body: {
-    type: 'object',
-    properties: {
-      name: { type: 'string', minLength: 1 },
-      logo: { type: 'string' },
-      enabled: { type: 'boolean' }
-    },
-    required: ['name', 'logo', 'enabled']
-  }
-}
-
-const updateOne = {
-  body: {
-    type: 'object',
-    properties: {
-      name: { type: 'string', minLength: 1 },
-      logo: { type: 'string' },
-      enabled: { type: 'boolean' }
-    },
-    required: ['name', 'logo', 'enabled']
-  },
-  params: {
-    type: 'object',
-    properties: {
-      id: { type: 'integer' }
-    }
-  }
-}
-
-const deleteOne = {
-  params: {
-    type: 'object',
-    properties: {
-      id: { type: 'integer' }
-    }
-  }
-}
+const findAll = schema.findAll
+const findOne = schema.findOne
+const insertOne = schema.insertOne
+const updateOne = schema.updateOne
+const deleteOne = schema.deleteOne
 
 module.exports = { findAll, findOne, insertOne, updateOne, deleteOne }
