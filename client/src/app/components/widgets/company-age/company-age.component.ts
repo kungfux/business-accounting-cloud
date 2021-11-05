80cc10eb56a62b555b6489dd6d8dfa22795fb03d
kungfux <alexander.v.fuks@gmail.com>
2021-11-05 14:57:41 +0300

    Add company age widget

---
+++ client/src/app/components/widgets/company-age/company-age.component.ts
@@ -0,0 +1,62 @@
+import { AfterViewInit, Component } from '@angular/core';
+import { CompanyApiService } from 'src/app/services/api/company.service';
+import { Company } from 'src/app/services/api/models/company';
+import { UserPreferencesService } from 'src/app/services/userPreferences.service';
+
+@Component({
+  selector: 'app-company-age',
+  templateUrl: './company-age.component.html',
+  styleUrls: ['./company-age.component.css'],
+})
+export class CompanyAgeComponent implements AfterViewInit {
+  isLoading: boolean = true;
+  company?: Company = undefined;
+  companyAge: string | undefined = undefined;
+
+  constructor(
+    private companyApi: CompanyApiService,
+    public userPreferences: UserPreferencesService
+  ) {}
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.getCompanyAge();
+    }, 100);
+  }
+
+  getCompanyAge(): void {
+    if (this.userPreferences.companyId) {
+      this.companyApi.getCompany(this.userPreferences.companyId).subscribe({
+        next: (company) => {
+          this.company = company;
+          this.companyAge = this.getAge(company.created!);
+          this.isLoading = false;
+        },
+      });
+    }
+  }
+
+  getAge(created: Date): string {
+    var today = new Date();
+    var birthDate = new Date(created);
+    var years = today.getFullYear() - birthDate.getFullYear();
+    var months = today.getMonth() - birthDate.getMonth();
+    var days = today.getDay() - birthDate.getDay();
+    if (years > 0) {
+      if (years == 1) return `${years} год`;
+      if (years < 5) return `${years} года`;
+      if (years >= 5) return `${years} лет`;
+    }
+    if (months > 0) {
+      if (months == 1) return `${months} месяц`;
+      if (months < 5) return `${months} месяца`;
+      if (months >= 5) return `${months} месяцев`;
+    }
+    if (days > 0) {
+      if (days == 1) return `${days} день`;
+      if (days < 5) return `${days} дня`;
+      if (days >= 5) return `${days} дней`;
+    }
+    return '0 дней';
+  }
+}
