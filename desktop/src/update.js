fc0e177a03feb925d1eaa7dac0ac2b3d778855d9
kungfux <alexander.v.fuks@gmail.com>
2021-11-24 03:45:59 +0300

    Add auto update

---
+++ desktop/src/update.js
@@ -0,0 +1,36 @@
+const { app, dialog, BrowserWindow } = require('electron')
+const { autoUpdater } = require("electron-updater");
+
+class Update {
+
+    constructor() { }
+
+    initAutoUpdate() {
+        app.whenReady().then(() => {
+            autoUpdater.on('update-available', (ev, info) => {
+                dialog.showMessageBox(
+                    BrowserWindow.getAllWindows()[0],
+                    {
+                        title: 'Доступно обновление',
+                        message: 'Сейчас будет загружена новая версия программы.\r\nЭто может занять какое-то время.',
+                        type: 'info'
+                    })
+            })
+
+            autoUpdater.on('update-downloaded', (ev, info) => {
+                dialog.showMessageBoxSync(
+                    BrowserWindow.getAllWindows()[0],
+                    {
+                        title: 'Доступно обновление',
+                        message: 'Сейчас будет установлена новая версия программы.',
+                        type: 'info'
+                    })
+                autoUpdater.quitAndInstall(false, true);
+            });
+
+            autoUpdater.checkForUpdatesAndNotify()
+        })
+    }
+}
+
+module.exports = { Update }
---
+++ hostSetup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ./desktop
+npm run server
