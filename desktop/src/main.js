9f295f7d329bb43cc7beb6e930834e9e365a8994
kungfux <alexander.v.fuks@gmail.com>
2021-11-26 14:37:59 +0300

    Inject script to check app context

--- desktop/src/main.js
+++ desktop/src/main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, Menu } = require('electron')
 const { Update } = require('./update')
 const { MainMenu } = require('./main-menu')
+const path = require('path')
 
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
@@ -9,6 +10,7 @@ const createWindow = () => {
         height: 1000,
         webPreferences: {
             sandbox: true,
+            preload: path.join(__dirname, 'preload.js')
         }
     })
 
@@ -18,6 +20,8 @@ const createWindow = () => {
 app.whenReady().then(() => {
     createWindow()
 
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
